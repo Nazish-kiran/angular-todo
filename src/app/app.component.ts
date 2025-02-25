c8b877975a8a021a4d9b050313683f1f1cda113e
NazishKiran <nazishkirancosmos123@gmail.com>
2025-02-25 12:50:10 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AddTaskComponent } from './add-task/add-task.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AddTaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
